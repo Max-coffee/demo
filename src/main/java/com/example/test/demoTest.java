934b531c66cd107c0ec9d2f7ae7d7cf36527bae8
max <1435137603@qq.com>
2023-07-10 20:32:57 +0800

    First commit

---
+++ src/main/java/com/example/test/demoTest.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+/**
+ * @ClassName demoTest
+ * @Description TODO
+ * @Author A.Shatter
+ * @Date 2023/7/10 20:28
+ * @Version 1.0
+ */
+public class demoTest {
+    public int num;
+}
